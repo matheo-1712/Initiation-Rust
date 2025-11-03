b655d459e34d7c1879c3d841b0ff66433018c0e8
matheo-1712 <perod.matheo@gmail.com>
2025-11-03 19:57:17 +0100

    Ajout d’un mini-jeu de devinette dans `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,41 @@
+use std::io;
+
 // main.rs
-fn main() {println!("Hello, world!"); }
+fn main() { numbers();}
+
+fn numbers() {
+
+    let nombre = 10;
+    println!("Devinez le nombre !");
+
+    loop {
+        println!("Veuillez entrer un nombre :");
+
+        let mut saisie = String::new();
+
+        io::stdin()
+            .read_line(&mut saisie)
+            .expect("Échec de la lecture de l'entrée utilisateur");
+
+        // Convertit la saisie en i32
+        let supposition: i32 = match saisie.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Veuillez entrer un nombre valide !");
+                continue; // redemande une saisie
+            }
+        };
+
+        if supposition < nombre {
+            println!("Trop petit !");
+        } else if supposition > nombre {
+            println!("Trop grand !");
+        } else if supposition == nombre {
+            println!("Bravo ! Vous avez trouvé 🎉");
+            break;
+        } else {
+            println!("Erreur !");
+            break;
+        }
+    }
+}
