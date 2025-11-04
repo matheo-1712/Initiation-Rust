04d8e469fb239f17973e9d3ffe73e4c57edd97b9
matheo-1712 <perod.matheo@gmail.com>
2025-11-04 09:57:40 +0100

    Refactorisation : extraction du menu principal dans `main_menu.rs` et nettoyage de `main.rs`. Ajout de la configuration IntelliJ IDEA.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,5 @@
-include!("initiation/jeu_nombres.rs");
-include!("initiation/variables.rs");
-
+include!("main_menu.rs");
 // main.rs
 fn main() {
-    loop {
-        println!("=== MENU ===");
-        println!("1. Jeu de devinette (numbers)");
-        println!("2. Variables (variables)");
-        println!("3. Quitter");
-
-        let mut choix = String::new();
-        println!("Entrez votre choix :");
-        io::stdin()
-            .read_line(&mut choix)
-            .expect("Erreur de lecture");
-
-        let choix = choix.trim(); // enlever \n et espaces
-
-        match choix {
-            "1" => numbers(),
-            "2" => variables(),
-            "3" => {
-                println!("Au revoir !");
-                break; // sortir de la boucle et terminer le programme
-            }
-            _ => println!("Choix invalide, réessayez."),
-        }
-    }
+    main_menu()
 }
